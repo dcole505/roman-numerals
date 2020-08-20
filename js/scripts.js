1d6580a94cf4eb2754c9bd5e981dbb686e8058b1
David Cole <davidtcole@yahoo.com>
2020-08-20 13:53:19 -0700

    finalize the refactoring of our recursive toRoman function into a non-recursive function which achieves the same results..
    Co-authored-by: Alex Goodwin <hagoodwi@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     let number = $("input#input1").val();
     
     if (checkInput(number)){
-      $("#result1").text(toRoman(number));
+      $("#result1").text(toRoman2(number));
     }else{
       alert("Please enter an integer between 1 - 3999");
     }
@@ -28,19 +28,19 @@ function validNumber(number) {
   return true;
 }
 
-function toRoman(number){
-  const decimals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-  const roman = ["M","CM","D","CD","C","XC","L", "XL", "X", "IX", "V", "IV", "I"];
+// function toRoman(number){
+//   const decimals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+//   const roman = ["M","CM","D","CD","C","XC","L", "XL", "X", "IX", "V", "IV", "I"];
   
-  for (let i = 0; i < decimals.length; i++) {
-    if(number < 1)
-        return "";       
+//   for (let i = 0; i < decimals.length; i++) {
+//     if(number < 1)
+//         return "";       
 
-    if(number >= decimals[i]) {
-        return roman[i] + toRoman(number - decimals[i]);        
-    }
-}
-}
+//     if(number >= decimals[i]) {
+//         return roman[i] + toRoman(number - decimals[i]);        
+//     }
+//   }
+// }
 
 function toRoman2(number){
   let outputString = "";
@@ -49,19 +49,16 @@ function toRoman2(number){
 
   for (let i = 0; i < decimals.length; i++) {
     if(number >= decimals[i]){
-      if (number > 0){
-        number = number - decimals[i]; //ex 55-50 = 5
-        //const numeral1 = decimals[i];
-        alert(roman[i]);
-      }else{
-        outputString += roman[i];
-        //alert(outputString);
+      if (5 <= number && number <= 8){
+        number -= 5;
+      }else if(1 <= number && number <= 3){
+        number -= 1;
+      }else {
+        number -= decimals[i];
       }
+      outputString += roman[i];
+      i--;
     }
-  //return outputString;
   }
+  return outputString;
 }
-  
-  //const firstElement = roman.shift(number);
-  //number = toString(number.split(''));
-  //alert(number);
